ec3ed403445514f4a7b69e708ba7d872f1a431aa
anand738 <anandgupta73830@gmail.com>
2026-06-13 21:44:56 +0530

    Initial Commit

--- 2.ChatModels/2.chatmodel_groq.py
+++ 2.ChatModels/2.chatmodel_groq.py
@@ -2,7 +2,7 @@ from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 load_dotenv()
 
-llm = ChatGroq(model="qwen/qwen3-32b")
+llm = ChatGroq(model="llama-3.3-70b-versatile")
 
 result = llm.invoke("Who is prime minister of india?")
 
